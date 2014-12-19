dab38c1ee12a162fda526f0d70f10f85799e0b6e
Mihail Kornilov <fix-06@yandex.ru>
2014-12-19 21:51:59 +0300

    base state of site

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=kinder_menu',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'adminEmail' => 'fix-06@yandex.ru',
+];
